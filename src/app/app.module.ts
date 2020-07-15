d33fdfc1180477fa4ac48fbfe4969fefb64960ea
karora1998 <karora1_be16@thapar.edu>
2020-07-15 16:29:03 +0530

    Added the header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,48 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { faUserPlus, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  animations: [
+  	trigger('openClose', [
+        transition('closed => open', [
+            style({ opacity: 0, transform: 'translateY(-30px)'}),
+            animate('150ms ease-out',
+                style({opacity: '*', transform: 'translateY(0)'}))
+        ]),
+        transition('open => closed', [
+            style({ opacity: '*', transform: 'translateY(0)'}),
+            animate('150ms ease-out',
+                style({opacity: 0, transform: 'translateY(-30px)'}))
+        ])
+    ])
+  ]
+})
+export class HeaderComponent {
+  isAuthenticated: boolean = false;
+  faUserPlus = faUserPlus;
+  faShoppingCart = faShoppingCart;
+  isOpen: boolean = false;
+  name: string = 'User';
+  @ViewChild('myDropDown') dropdown: ElementRef;
+
+  openChange() {
+  	if(!this.dropdown.nativeElement.classList.contains('show')){
+  		this.isOpen = true;
+  	}
+    else {
+    	this.isOpen = false;
+    }
+  }
+
+  onCartClicked() {
+
+  }
+
+  onSignIn() {
+  	this.isAuthenticated = true;
+  }
+
+}
